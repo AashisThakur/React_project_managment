362c314b93671c6673ccc6455da4a9c352af43b8
AashisThakur <aashishthakur3069@gmail.com>
2024-03-31 23:10:12 +0530

    check

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React_project_managment/",
+  base: "/",
   plugins: [react()],
 })
